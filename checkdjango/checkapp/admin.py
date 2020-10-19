28b43ef066f3b2c2ad5ade285e65f52bb361eeba
plagueevgeny <PlagueEvgeny@yandex.ru>
2020-10-19 11:50:44 +0500

    Check

--- checkdjango/checkapp/admin.py
+++ checkdjango/checkapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from checkapp.models import RockCategory
+from checkapp.models import Group
+from checkapp.models import Album
+from checkapp.models import Product
+from checkapp.models import Clips
+
+
+admin.site.register(RockCategory)
+admin.site.register(Group)
+admin.site.register(Album)
+admin.site.register(Product)
+admin.site.register(Clips)
--- checkdjango/checkapp/models.py
+++ checkdjango/checkapp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class RockCategory(models.Model):
+    name = models.CharField(max_length=128)
+    desc = models.TextField(blank=True)
+
+class Group(models.Model):
+    category = models.ForeignKey(RockCategory,
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    desc = models.TextField(blank=True)
+    age = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+
+class Album(models.Model):
+    category = models.ForeignKey(Group,
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    desc = models.TextField(blank=True)
+    NumberOfSongs = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+
+class Product(models.Model):
+    category = models.ForeignKey(Group,
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    desc = models.TextField(blank=True)
+    hours = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+
+class Clips(models.Model):
+    category = models.ForeignKey(Group,
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    desc = models.TextField(blank=True)
+    hours = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
