5acf1c700f38a9cf999f11838b9b43b70539936d
plagueevgeny <PlagueEvgeny@yandex.ru>
2020-11-14 09:14:58 +0500

    HW

--- checkdjango/authapp/views.py
+++ checkdjango/authapp/views.py
@@ -1,15 +1,43 @@
-from django.contrib.auth.forms import AuthenticationForm
+import django.contrib.auth as auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-from authapp.forms import LoginForm
+from authapp.forms import LoginForm, RegisterForm
 
 
 def login(request):
-    # form = AuthenticationForm()
-    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            auth.login(request, form.get_user())
+            return HttpResponseRedirect('/')
+    else:
+        form = LoginForm()
 
     context = {
         'page_title': 'авторизация',
         'form': form,
     }
     return render(request, 'authapp/login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect('/')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = RegisterForm()
+
+    context = {
+        'page_title': 'регистрация',
+        'form': form,
+    }
+    return render(request, 'authapp/register.html', context)
---
+++ checkdjango/checkapp/urls.py
@@ -0,0 +1,33 @@
+"""mydjangoapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+import checkapp.views as checkapp
+
+
+from django.contrib import admin
+from django.urls import path, include
+
+app_name = 'checkapp'
+
+urlpatterns = [
+    path('', checkapp.index),
+    path('catalog/category/<int:pk>/', checkapp.group, name='group'),
+    path('catalog/category/album/<int:pk>/', checkapp.album, name='album'),
+    path('catalog/category/album/song/<int:pk>/', checkapp.song, name='song'),
+    path('catalog/category/album/song/song_page/<int:pk>/', checkapp.song_page, name='song_page'),
+    path('catalog/', checkapp.catalog),
+    path('admin/', admin.site.urls),
+
+]
