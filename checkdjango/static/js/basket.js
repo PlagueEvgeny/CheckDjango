319d93a81fd24292ef2566409b12728943bd7e36
plagueevgeny <PlagueEvgeny@yandex.ru>
2020-11-16 10:13:56 +0500

    SongBasket

--- checkdjango/static/js/basket.js
+++ checkdjango/static/js/basket.js
@@ -7,7 +7,7 @@ window.onload = function () {
             url: event.target.href,
             success: function (data) {
                 // console.log('response', data);
-                $('.basket-item-' + data.songe_basket_id).remove();
+                $('.songe-item-' + data.songe_basket_id).remove();
             }
         })
     })
